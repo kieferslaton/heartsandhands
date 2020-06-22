e7146054c13587ffe9b889b26c2a18d63874311d
Kiefer Slaton <kieferslaton@Kiefers-MacBook-Pro.local>
2020-06-21 19:08:59 -0500

    Footer Adjustments

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,10 +19,12 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle="{data.site.siteMetadata.title}" />
         <main>{children}</main>
-      <footer>
-        <div className="container-fluid border-top border-dark">
+      <footer className="mt-5 mx-0 px-0">
+      <div className="divider m-0 p-0">
+          </div>
+        <div className="container-fluid mx-0">
           <div style={{'font-size': '1em','line-height': '1em'}} className="row justify-content-center">
-            <div className="col-12 col-md-3 my-md-5 mt-3 mb-2 text-center footer-address">
+            <div className="col-12 col-md-3 my-md-4 mt-3 mb-2 text-center footer-address">
               <p className="font-weight-bold m-2">HEARTS AND HANDS OF BAYTOWN</p>
               <p className="m-2">A ministry of Iglesia Cristo Viene</p>
               <address className="m-2">
@@ -30,14 +32,14 @@ const Layout = ({ children }) => {
                 Baytown, TX 77520
               </address>
             </div>
-            <div className="col-12 col-md-3 my-md-5 mb-5 text-center footer-donate">
+            <div className="col-12 col-md-3 my-md-4 mb-4 text-center footer-donate">
               <p className="font-weight-bold pt-3 pt-md-0">DONATE</p>
               <p>Every $1 donated provides enough food for 4 meals.</p>
               <Link to="/donate"><button class="btn">DONATE NOW</button></Link>
             </div>
           </div>
           <div className="row">
-            <div className="col-12 text-center bg-dark">
+            <div className="col-12 text-center">
               <p className="m-0 py-1" style={{'color':'white', 'font-size':'0.7em'}}>&copy; Hearts and Hands of Baytown. Site by Kiefer Slaton</p>
             </div>
           </div>
