b67fcce7b92d457ccd24c18bde1be14b9a3e7e49
Kiefer Slaton <kieferslaton@Kiefers-MacBook-Pro.local>
2020-07-09 12:54:04 -0500

    First commit

--- src/apikeys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    CALENDAR_ID: "45rbm4dttbq196qhgc9rkuur4c@group.calendar.google.com",
-    CALENDAR_API: "AIzaSyDaXiyGvW2bfOIkrM6wLWMnNhgK25POl7w", 
-    STRIPE_API: "pk_test_51GvjsmGJc8kSpHFEbHieh4gp0n0NoORy1LLt9u7y5wJ4gFmLSmZALU0Qjyb88bYAN4dWtwHoY4YYVAcZtFqxpBsG00x3ZpbIdX"
-}
