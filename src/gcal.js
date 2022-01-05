a74a78b08f6727af7b437422b21dc5f1ef149ced
kieferslaton <45370935+kieferslaton@users.noreply.github.com>
2022-01-05 15:46:13 -0600

    Update gcal.js

--- src/gcal.js
+++ src/gcal.js
@@ -11,6 +11,7 @@ export function getEvents(callback) {
     if (!err) {
       const events = []
       JSON.parse(resp.text).items.map(event => {
+        console.log(event.start);
         events.push({
           id: event.id,
           start: event.start.date || event.start.dateTime,
